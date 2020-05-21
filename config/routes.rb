98fa14db85487dc374fc951741850e6caf890f9d
clement lemaire <c.lemaire027@gmail.com>
2020-05-21 21:09:22 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'index', to: 'restaurants#index'
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new', as: :new
+  get 'restaurants/:id', to: 'restaurants#show', as: :task
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id/reviews', to: 'restaurants#new', as: :new_review
+  post 'restaurants/:id/reviews/new', to: 'restaurants#create'
 end
